10c0424d10a5a6b404e60635e88fd891e65efe3e
Dmitry Gordeev <dmitrijg934@gmail.com>
2024-07-02 17:46:03 +0300

    Initial commit - Spring application has been created with required dependencies.

---
+++ src/test/java/nn/dgordeev/taco/TacoCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nn.dgordeev.taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
