80676a2b2d5ac212a24c3218a41e137a5876ee97
Dmitry Gordeev <dmitrijg934@gmail.com>
2024-07-04 11:55:13 +0300

    Unit-test fix.

--- src/test/java/nn/dgordeev/taco/controller/HomeControllerTest.java
+++ src/test/java/nn/dgordeev/taco/controller/HomeControllerTest.java
@@ -1,9 +1,11 @@
 package nn.dgordeev.taco.controller;
 
+import nn.dgordeev.taco.repository.IngredientRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -12,12 +14,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@DisplayName("Тесты на проверку главной страницы")
 @WebMvcTest(HomeController.class)
+@DisplayName("Тесты на проверку главной страницы")
 class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @SpyBean
+    private IngredientRepository ingredientRepository;
 
     @Test
     @DisplayName("Главная страница отдаёт корректное представление")
